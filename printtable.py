19e2025fd018e0a4bc0fc17dd39b8dcad22ebc3b
Cecilia Furugård <cecilia.furugard@gmail.com>
2019-09-11 14:52:26 +0200

    both old and new script

--- printtable.py
+++ printtable.py
@@ -1,3 +1,9 @@
+def multtable(number, limit):
+    for i in range(1, limit+1):
+        print(i * number)
+
+#multtable(7, 10)
+
 def powertable(number, limit):
     for i in range(1, limit+1):
         print(i ** number)
